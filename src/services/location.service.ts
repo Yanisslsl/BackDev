4714b24b33078d7c5851a805eb366633d852103f
Yaniss <yaniss@wikodit.fr>
2022-05-16 00:40:16 +0200

    ALL

---
+++ src/services/location.service.ts
@@ -0,0 +1,17 @@
+const findDistance = (cordA: any, cordB: any) => {
+  const lon1 = (cordA.longitude * Math.PI) / 180;
+  const lon2 = (cordB.longitude * Math.PI) / 180;
+  const lat1 = (cordA.latitude * Math.PI) / 180;
+  const lat2 = (cordB.latitude * Math.PI) / 180;
+  const dlon = lon2 - lon1;
+  const dlat = lat2 - lat1;
+  const a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+  const c = 2 * Math.asin(Math.sqrt(a));
+  const r = 6371;
+  return c * r;
+};
+
+export default findDistance;
